3935104db69033158ce7ebf7a89bb5adc0320999
cyzhao <471038339@qq.com>
2019-03-25 20:25:14 +0800

    test pass aabb

--- src/test/test_aabb.cc
+++ src/test/test_aabb.cc
@@ -1,38 +1,37 @@
 #include <vector>
 #include "aabb.h"
 #include <iostream>
-#define TINYOBJLOADER_IMPLEMENTATION
-#define TINYOBJLOADER_USE_DOUBLE
-#include "tiny_obj_loader.h"
+#include "model.h"
 #include <Eigen/Core>
 using namespace std;
 using namespace Eigen;
 
-string dirname(const string &s) {
-  unsigned long find = s.find_last_of('/');
-  if (find == std::string::npos)return {"."};
-  return s.substr(0, find);
-}
+
 
 
 int main(int argc, char** argv){
-  vector<tinyobj::shape_t> shapes;
-  vector<tinyobj::material_t> materials;
-  tinyobj::attrib_t attrib;
-  string filename = argv[1];
-  string err;
-  if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &err, // triangulated == true
-                        filename.c_str(), (dirname(filename) + "/").c_str())) {
-    throw runtime_error(err.c_str());
-  }
+  const string filename = argv[1];
+  Scene scene(filename.c_str());
 
+  for(auto& model_ptr : scene.models){
+    size_t num_tris = model_ptr->tris_.size();
+    vector<tri_aabb> aabbs(num_tris);
 
-  Vector3d p1, p2, p3;
-  for(auto& one_shape : shapes){
-    cout << one_shape.name << endl;
-    size_t num_tris = one_shape.mesh.material_ids.size();
-    cout << num_tris << endl;
+    #pragma omp parallel for
+    for(size_t i = 0; i < num_tris; ++i){
+      aabbs[i] = tri_aabb(model_ptr->tris_[i].mtl_id_, model_ptr->tris_[i].p_.col(0),model_ptr->tris_[i].p_.col(1), model_ptr->tris_[i].p_.col(2));
+    }
+    #pragma omp parallel for    
+    for(size_t i = 0; i < num_tris; ++i){
+      for(size_t j = 0; j < 3; ++j){
+        for(size_t k = 0; k < 3; ++k){
+          assert(model_ptr->tris_[i].p_(k, j) <= aabbs[i].up_bd_(k) &&
+                 model_ptr->tris_[i].p_(k, j) >= aabbs[i].low_bd_(k));
+        }
+      }
+    }
   }
+  cout <<"pass" << endl;
 
   return 0;
 }
