445f98bc9c04545618203b418f44555741f29465
cyzhao <471038339@qq.com>
2019-03-27 11:31:59 +0800

    pass compliaton

--- src/ray.cpp
+++ src/ray.cpp
@@ -5,7 +5,7 @@ using namespace Eigen;
 
 Ray::Ray(const vec& origin, const vec& dire):  origin_(origin), dire_(dire){}
 
-enum axis{x, y, z;}
+enum axis{x, y, z};
 bool project_dim(const Eigen::Vector3d& normal, axis& dim1, axis& dim2){
   dim1 = x;
   dim2 = y;
@@ -31,10 +31,10 @@ bool project_dim(const Eigen::Vector3d& normal, axis& dim1, axis& dim2){
 
 }
 
-bool get_cross_point(const vec& ori, const vec& dir, const vec& norm, const double& d, vec& offset ){
+bool get_cross_point(const vec& ori, const vec& dir, const vec& norm, const double& d, double& offset ){
   
   const double deno = norm(0) * dir(0) + norm(1) * dir(1) + norm(2) * dir(2);
-  if(fabs(deno) < 1e -5)
+  if(fabs(deno) < 1e-5)
     return false;
   else{
     offset = ( -d - norm(0) * ori(0) - norm(1) * ori(1) - norm(2) * ori(2) ) * (1 / deno);
@@ -51,7 +51,7 @@ bool get_cross_point(const vec& ori, const vec& dir, const vec& norm, const doub
 
 bool Ray::intersect_aabb(const aabb& bd_box_){
   bool is_inter = false;
-#pragma omp parallel for
+
   for(size_t dim = 0; dim < 3; ++dim){
     vec normal;
     double para_d;
@@ -88,8 +88,8 @@ bool Ray::intersect_aabb(const aabb& bd_box_){
 bool Ray::intersct_tri_aabb(const tri_aabb& tri, vec&cross_point){
   axis dim1, dim2;
   double offset;
-  if(!project_dim(plane_normal, dim1, dim2)
-     || !get_cross_point(origin_, dire_, tri.normal_, tri.d, offest))
+  if(!project_dim(tri.normal_, dim1, dim2)
+     || !get_cross_point(origin_, dire_, tri.normal_, tri.d_, offset))
     return false;
 
   cross_point = origin_ + dire_ * offset;
@@ -103,12 +103,10 @@ bool Ray::intersct_tri_aabb(const tri_aabb& tri, vec&cross_point){
       ((tri.p_(dim1, 0) - tri.p_(dim1, 2)) * (tri.p_(dim2, 1) - tri.p_(dim2, 2))
        - (tri.p_(dim2, 0) - tri.p_(dim2, 2)) * (tri.p_(dim1, 1) - tri.p_(dim1, 2)));
   
-  // cout<< " check through : " << s << " " << t << " " << 1 - s - t << endl;
-  if(s != s){
-    cout << "nan" << endl << dim1 << dim2 << endl<< cross_point << endl << endl << tri.p_;
-    cout << " jerer" << endl;
-    assert(s == s);
-  }
+
+
+  assert(s == s);
+  
 
   // cout << tri.p_ << endl;
   return ( s > 0 && s < 1 && t > 0 && t < 1 && (1 - s - t) > 0 && (1 - s - t) < 1 );
@@ -123,19 +121,19 @@ bool Ray::intersect(const unique_ptr<KD_tree_tris>& kd, size_t& face_id, vec& cr
       assert(kd->left_tree_ == nullptr);
       assert(kd->right_tree_ == nullptr);
       bool is_inter = false;
-#pragma omp parallel for
+
       for(size_t f_id = 0; f_id < kd->child_.size(); ++f_id){
         if(is_inter = intersct_tri_aabb(*(kd->child_[f_id]), cross_point)){
-          face_id = kd->child_[f_id].id_;
+          face_id = kd->child_[f_id]->id_;
           break;
         }
 
       }
-      return is_iter;
+      return is_inter;
     }
     else
-      return intersct(kd->left_tree_, face_id, cross_point)
-          || intersct(kd->right_tree_, face_id, cross_point);
+      return intersect(kd->left_tree_, face_id, cross_point)
+          || intersect(kd->right_tree_, face_id, cross_point);
   }
   else{
     return false;
---
+++ src/test/test_ray.cc
@@ -0,0 +1,8 @@
+#include "ray.h"
+#include <iostream>
+using namespace std;
+using namespace Eigen;
+int main(int argc, char** argv){
+  return 0;
+}
+
